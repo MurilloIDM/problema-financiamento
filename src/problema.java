04e0b536a71f815d1420746b5d29c0821f06eb96
Murillo Isidoro de Medeiros <44484530+MurilloIDM@users.noreply.github.com>
2020-05-22 19:56:37 -0300

    Leitura do financiamento

--- src/problema.java
+++ src/problema.java
@@ -8,8 +8,9 @@ public class problema {
 		Locale.setDefault(Locale.US);
 		Scanner input = new Scanner(System.in);
 		
-		String nomeCliente;
-		double salario;
+		String nomeCliente, porcentagem;
+		double salario, totalFinanciado;
+		int numeroPrestacoes;
 		
 		System.out.println("DIGITE OS DADOS DO FINANCIAMENTO: ");
 		System.out.print("Nome do cliente: ");
@@ -18,8 +19,20 @@ public class problema {
 		salario = input.nextDouble();
 		
 		while (salario < 0.0) {
-			System.out.print("Salario: ");
+			System.out.print("Digite uma valor para salario positivo. Tente novamente: ");
 			salario = input.nextDouble();
+		}
+		
+		System.out.print("Numero de prestacoes: ");
+		numeroPrestacoes = input.nextInt();
+		System.out.print("Porcentagem de entrada: ");
+		porcentagem = input.next();
+		System.out.print("Valor total financiado: ");
+		totalFinanciado = input.nextDouble();
+		
+		while ((0.2 * totalFinanciado) / numeroPrestacoes > (salario * 0.30)) {
+			System.out.print("Valor invalido. Tente novamente: ");
+			totalFinanciado = input.nextDouble();
 		}
 		
 		
